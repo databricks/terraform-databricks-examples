d73760a561505e5f902aeaa5ddd8401fdec3b91d
AleksCallebat <aleksander.callebat@gmail.com>
2023-03-21 11:31:53 +0100

    terraform with gcp support

--- examples/gcp-lakehouse/variables.tf
+++ examples/gcp-lakehouse/variables.tf
@@ -1,30 +1,23 @@
 variable "databricks_account_id" {
-  default = "f187f55a-9d3d-463b-aa1a-d55818b704c9"
 }
 variable "databricks_google_service_account" {
   # Input the service account email adress generated via gcp-sa-provisionning
-    default = "aleks421-sa2@fe-dev-sandbox.iam.gserviceaccount.com"
 }
 
 
 variable "google_project" {
-  default = "fe-dev-sandbox"
 }
 variable "google_region" {
-    default = "europe-west1"
 }
 variable "google_zone" {
-    default = "europe-west1-a"
 }
 variable "prefix" {
-    default = "aleks421"
 }
 
 
 variable "delegate_from" {
  description = "Allow either user:user.name@example.com, group:deployers@example.com or serviceAccount:sa1@project.iam.gserviceaccount.com to impersonate created service account"
  type        = list(string)
- default = [ "user:aleksander.callebat@databricks.com" ]
 }
 
 
--- examples/gcp-sa-provisionning/variables.tf
+++ examples/gcp-sa-provisionning/variables.tf
@@ -1,23 +1,17 @@
 variable "databricks_account_id" {
-  default = "f187f55a-9d3d-463b-aa1a-d55818b704c9"
 }
 
 variable "google_project" {
-  default = "fe-dev-sandbox"
 }
 variable "google_region" {
-    default = "europe-west1"
 }
 variable "google_zone" {
-    default = "europe-west1-a"
 }
 variable "prefix" {
-    default = "aleks421"
 }
 
 
 variable "delegate_from" {
  description = "Allow either user:user.name@example.com, group:deployers@example.com or serviceAccount:sa1@project.iam.gserviceaccount.com to impersonate created service account"
  type        = list(string)
- default = [ "user:aleksander.callebat@databricks.com" ]
 }
