7e2b244a34c02ecfb16837d695902c1091051f36
Nikolay Ulmasov <ulmasov@hotmail.com>
2023-03-25 22:13:48 +0000

    clean up

--- examples/adb-with-private-link-standard/outputs.tf
+++ examples/adb-with-private-link-standard/outputs.tf
@@ -1,9 +1,5 @@
-output "databricks_workspace_url" {
-  value = module.adb-with-private-link-standard.dp_workspace_url
-}
-
 output "test_vm_password" {
-  description = "Password to access the VM, use `terraform output -json test_vm_password` to get the password value"
+  description = "Password to access the Test VM, use `terraform output -json test_vm_password` to get the password value"
   value = module.adb-with-private-link-standard.test_vm_password
   sensitive = true
 }
--- modules/adb-with-private-link-standard/outputs.tf
+++ modules/adb-with-private-link-standard/outputs.tf
@@ -10,6 +10,7 @@ output "dp_workspace_url" {
 }
 
 output "test_vm_password" {
+  description = "Password to access the Test VM, use `terraform output -json test_vm_password` to get the password value"
   value = azurerm_windows_virtual_machine.testvm.admin_password
   sensitive = true
 }
