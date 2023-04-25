b965bd8d58dfbc312d77a6714b0fb4236b6ef64d
AleksCallebat <aleksander.callebat@gmail.com>
2023-04-25 17:20:24 +0200

    added gcp on new branch

--- examples/gcp-lakehouse/modules/gcp-byovpc/workspace.tf
+++ examples/gcp-lakehouse/modules/gcp-byovpc/workspace.tf
@@ -28,11 +28,4 @@ resource "databricks_mws_workspaces" "databricks_workspace" {
   depends_on = [google_compute_router_nat.nat]
 }
 
-output "databricks_host" {
-  value = databricks_mws_workspaces.databricks_workspace.workspace_url
-}
 
-output "databricks_token" {
-  value     = databricks_mws_workspaces.databricks_workspace.token[0].token_value
-  sensitive = true
-}
--- examples/gcp-lakehouse/modules/gcp-sa-provisionning/main.tf
+++ examples/gcp-lakehouse/modules/gcp-sa-provisionning/main.tf
@@ -137,10 +137,6 @@ resource "google_project_iam_custom_role" "workspace_creator" {
 
 data "google_client_config" "current" {}
 
-output "custom_role_url" {
-  value = "https://console.cloud.google.com/iam-admin/roles/details/projects%3C${data.google_client_config.current.project}%3Croles%3C${google_project_iam_custom_role.workspace_creator.role_id}"
-}
-
 resource "google_project_iam_member" "sa2_can_create_workspaces" {
   role    = google_project_iam_custom_role.workspace_creator.id
   member  = "serviceAccount:${google_service_account.sa2.email}"
