7c3914ab2949634d6b3e76a8736a3b126c791ef9
Pavan <pavanraju023@gmail.com>
2023-05-05 16:24:12 +1000

    Add time delay for databricks mws credential creation (#63)

--- modules/aws-exfiltration-protection/workspace.tf
+++ modules/aws-exfiltration-protection/workspace.tf
@@ -14,12 +14,20 @@ resource "databricks_mws_storage_configurations" "this" {
   storage_configuration_name = "${local.prefix}-storage"
 }
 
+## Adding 20 second timer to avoid Failed credential validation check
+resource "time_sleep" "wait" {
+  create_duration = "20s"
+  depends_on = [
+    aws_iam_role_policy.this
+  ]
+}
+
 resource "databricks_mws_credentials" "this" {
   provider         = databricks.mws
   account_id       = var.databricks_account_id
   role_arn         = aws_iam_role.cross_account_role.arn
   credentials_name = "${local.prefix}-creds"
-  depends_on       = [aws_iam_role_policy.this]
+  depends_on       = [time_sleep.wait]
 }
 
 resource "databricks_mws_workspaces" "this" {
