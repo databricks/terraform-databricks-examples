799889121fd7309d4ce2c65dda33dbec4975ef18
dfilonowicz <dfilonowicz@sii.pl>
2023-05-08 17:51:48 +0200

    Add workspace_resource_id for adb-lakehouse module

--- modules/adb-lakehouse/outputs.tf
+++ modules/adb-lakehouse/outputs.tf
@@ -33,6 +33,11 @@ output "workspace_id" {
   description = "ID of the Databricks workspace"
 }
 
+output "workspace_resource_id" {
+  value       = azurerm_databricks_workspace.this.id
+  description = "ID of the Databricks workspace resource"
+}
+
 output "workspace_url" {
   value       = azurerm_databricks_workspace.this.workspace_url
   description = "URL of the Databricks workspace"
