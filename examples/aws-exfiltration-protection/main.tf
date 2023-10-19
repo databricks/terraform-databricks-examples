4d911fa2a4bf0241ee516373efc4b5dfe0be4c5a
mwojtyczka <wojtyczka.marcin@gmail.com>
2023-10-19 22:13:50 +0200

    implemented code review feedback

--- examples/aws-exfiltration-protection/main.tf
+++ examples/aws-exfiltration-protection/main.tf
@@ -1,5 +1,5 @@
 module "aws-exfiltration-protection" {
-  source                      = "../../modules/aws-exfiltration-protection"
+  source                      = "github.com/databricks/terraform-databricks-examples/modules/aws-exfiltration-protection"
   databricks_account_id       = var.databricks_account_id
   databricks_account_username = var.databricks_account_username
   databricks_account_password = var.databricks_account_password
