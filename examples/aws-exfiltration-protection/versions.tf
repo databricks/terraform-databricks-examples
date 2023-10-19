08147355d4b920f048a46cb47b4c26cd7607fbe9
mwojtyczka <wojtyczka.marcin@gmail.com>
2023-10-19 23:09:47 +0200

    removed versions from examples

--- examples/aws-exfiltration-protection/versions.tf
+++ examples/aws-exfiltration-protection/versions.tf
@@ -2,12 +2,10 @@ terraform {
   required_providers {
     databricks = {
       source  = "databricks/databricks"
-      version = ">=1.27.0"
     }
 
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.20.1"
     }
   }
 }
