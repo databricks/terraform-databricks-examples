c19bb6d97e3aa4ece8ad3180cfd87e2dad16c434
Yessawabi <yassine.essawabi@databricks.com>
2024-06-18 11:38:18 +0800

    Removing Splunk TF example (#140)
    * Fix: Quick fix to solve some issues with adb-lakehouse example
    
    * Fix: Quick fix to solve some issues with adb-lakehouse example
    
    * Fix: Fixing Readme
    
    * Fix: Fixing Readme
    
    * Fix: Fixing Readme
    
    * Fix: Fixing Readme
    
    * Fix: Fixing Readme
    
    * Fix: Fixing Readme
    
    * Removing Splunk TF example

--- examples/adb-splunk/splunk_setup.sh
+++
@@ -1,5 +0,0 @@
-  #! /bin/bash
-  sudo apt update
-  sudo apt install docker.io -y
-  sudo apt install docker-compose -y
-  sudo docker run -d -p 8000:8000 -p 8089:8089 -p 9997:9997 -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=password" -e "SPLUNK_APPS_URL=https://adls5a0e9o.blob.core.windows.net/cnt1/databricks-add-on-for-splunk_110.tgz" --name splunk splunk/splunk:latest
