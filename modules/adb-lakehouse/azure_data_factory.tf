f86408eb9f50fd5a31a7725a42fe9b724be41589
micheledaddetta-databricks <michele.daddetta@databricks.com>
2024-11-22 16:34:14 +0100

    Fix resource group name value in the azure data factory resource

--- modules/adb-lakehouse/azure_data_factory.tf
+++ modules/adb-lakehouse/azure_data_factory.tf
@@ -3,6 +3,6 @@ resource "azurerm_data_factory" "adf" {
 
   name                = var.data_factory_name
   location            = local.rg_location
-  resource_group_name = var.location
+  resource_group_name = local.rg_name
   tags                = var.tags
 }
