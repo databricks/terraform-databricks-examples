9fa5dc0fbfc58a784b687f2c5482b846ff516dbd
Niko <ulmasov@hotmail.com>
2024-11-25 07:27:24 +0000

    restrict azurerm version in modules (#158)
    Signed-off-by: Niko <ulmasov@hotmail.com>

--- modules/adb-lakehouse/providers.tf
+++ modules/adb-lakehouse/providers.tf
@@ -2,6 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
+      version = ">=4.0.0"
     }
   }
 }
--- modules/adb-with-private-links-exfiltration-protection/providers.tf
+++ modules/adb-with-private-links-exfiltration-protection/providers.tf
@@ -3,6 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
+      version = ">=4.0.0"
     }
     random = {
       source = "hashicorp/random"
