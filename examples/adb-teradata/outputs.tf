81d3041ea67f992571b672e6d396745ed4e85ca5
Niko <ulmasov@hotmail.com>
2024-11-29 12:50:34 +0000

    Update adb-teradata to use azurerm v4
    Signed-off-by: Niko <ulmasov@hotmail.com>

--- examples/adb-teradata/outputs.tf
+++ examples/adb-teradata/outputs.tf
@@ -1,3 +1,18 @@
 output "pip" {
   value = module.test_vm_instance.vm_public_ip
 }
+
+output "azure_resource_group_id" {
+  description = "ID of the created Azure resource group"
+  value       = azurerm_resource_group.this.id
+}
+
+output "workspace_id" {
+  description = "The Databricks workspace ID"
+  value       = azurerm_databricks_workspace.this.workspace_id
+}
+
+output "workspace_url" {
+  description = "The Databricks workspace URL"
+  value       = azurerm_databricks_workspace.this.workspace_url
+}
