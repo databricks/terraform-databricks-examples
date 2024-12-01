290ab788b161202c084e6b6f2ad2ae043c7dd188
Niko <ulmasov@hotmail.com>
2024-12-01 11:01:43 +0000

    Update adb-teradata to use azurerm v4 (#164)
    Signed-off-by: Niko <ulmasov@hotmail.com>

--- examples/adb-teradata/data.tf
+++ examples/adb-teradata/data.tf
@@ -4,3 +4,7 @@ data "azurerm_client_config" "current" {
 data "external" "me" {
   program = ["az", "account", "show", "--query", "user"]
 }
+
+data "http" "my_public_ip" {
+  url = "https://ipinfo.io"
+}
