c0538f74ad45defec2f41a3ebbadcba86e621fdb
sysopmatt <mattpadams@gmail.com>
2024-12-22 20:56:37 -0800

    Rename module reference to not overlap with gcp-basic

--- examples/gcp-sa-provisioning/main.tf
+++ examples/gcp-sa-provisioning/main.tf
@@ -1,5 +1,5 @@
-module "gcp-basic" {
-  source         = "../modules/gcp-sa-provisioning"
+module "gcp-sa-provisioning" {
+  source         = "github.com/databricks/terraform-databricks-examples/modules/gcp-sa-provisioning"
   google_project = var.google_project
   prefix         = var.prefix
   delegate_from  = var.delegate_from
