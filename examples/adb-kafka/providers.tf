ab602e21e43c23c35df589ef5b5d78de0438c577
micheledaddetta-databricks <michele.daddetta@databricks.com>
2025-09-22 10:18:18 +0200

    Add required_version lower constraint for variable condition validation

--- examples/adb-kafka/providers.tf
+++ examples/adb-kafka/providers.tf
@@ -1,4 +1,7 @@
 terraform {
+
+  required_version = ">= 1.9.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
--- modules/adb-with-private-links-exfiltration-protection/providers.tf
+++ modules/adb-with-private-links-exfiltration-protection/providers.tf
@@ -1,5 +1,8 @@
 # versions.tf
 terraform {
+
+  required_version = ">= 1.9.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
