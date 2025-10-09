dd3e52cead7fe18274b119b0b88ca6be8ce084c7
Zach King <zcking@pm.me>
2025-10-09 12:29:01 -0500

    chore: update provider block comment

--- examples/aws-serverless-privatelink-to-cloud-service/providers.tf
+++ examples/aws-serverless-privatelink-to-cloud-service/providers.tf
@@ -5,7 +5,8 @@ provider "aws" {
   }
 }
 
-// initialize provider in "MWS" mode to provision new workspace
+// initialize an account-level provider to provision the
+// private connectivity endpoint rule on the NCC.
 provider "databricks" {
   host          = "https://accounts.cloud.databricks.com"
   account_id    = var.databricks_account_id
